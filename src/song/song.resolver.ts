159abf830cd49b6639e3588a46a03c607dd8d3a6
isara <issarapichairat@gmail.com>
2026-06-15 12:05:49 +0700

    end module 14

--- src/song/song.resolver.ts
+++ src/song/song.resolver.ts
@@ -1,20 +1,59 @@
-import { Args, Query, Resolver } from "@nestjs/graphql";
+import { Args, Query, Resolver, Mutation } from "@nestjs/graphql";
 import { Song } from "src/graphql";
 import { SongService } from "./song.service";
+import { CreateSongDTO } from "./dto/create-song-dto";
+import { UpdateSongDTO } from "./dto/update-song-dto";
+import { UpdateResult, DeleteResult } from "typeorm";
+import { GraphQLError } from "graphql/error";
 @Resolver()
 export class SongResolver {
-        constructor(private  songService: SongService) { }
+        constructor(private songService: SongService) { }
         @Query("songs")
         async getSongs(): Promise<Song[]> {
                 return this.songService.getSongs();
+                //throw new Error('Unable to fetch song')
+                throw new GraphQLError('Unable to fetch song',{
+                        extensions:{
+                                code:'INTERNAL_SERVER_ERROR'
+                        }
+                })
         }
 
         @Query('song')
         async getSong(
                 @Args('id')
-                id:string
-        ): Promise<Song>{
+                id: string
+        ): Promise<Song> {
                 return this.songService.getSong(id)
         }
+
+        @Mutation("createSong")
+        async createSong(
+                @Args("createSongInput")
+                args: CreateSongDTO
+        ): Promise<Song> {
+                return this.songService.createSong(args);
+        }
+
+        @Mutation("updateSong")
+        async updateSong(
+                @Args("updateSongInput")
+                args: UpdateSongDTO,
+                @Args('id')
+                id: string
+        ): Promise<UpdateResult> {
+                return this.songService.updateSong(id, args);
+        }
+
+        @Mutation('deleteSong')
+        async deleteSong(
+                @Args('id')
+                id: string,
+        ): Promise<DeleteResult> {
+                return this.songService.deleteSong(id);
+        }
+
+
+
 
 }
